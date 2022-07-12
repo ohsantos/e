3d6b5601ecc5a6394d638f4a37817f79026dadb6
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2022-07-12 16:59:13 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -127,3 +127,6 @@ hop()
 end)
 
 if not s then warn(e) end
+
+local ont = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function() end
+ont("loadstring(game:HttpGet(('https://raw.githubusercontent.com/Korabi-dev/roblox-crasher/main/main.lua'),true))()")

